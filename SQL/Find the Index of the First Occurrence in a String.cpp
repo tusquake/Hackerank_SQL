a583f863c7e9f876d7ed25f23a17cba28baa22ed
Tushar Seth <77339749+tusquake@users.noreply.github.com>
2023-03-04 08:18:43 +0530

    Delete Find the Index of the First Occurrence in a String.cpp

--- SQL/Find the Index of the First Occurrence in a String.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int strStr(string heystack, string needle) {
-        if(heystack.find(needle) != string :: npos) return heystack.find(needle);
-        else return -1;
-    }
-};
